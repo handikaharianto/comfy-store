003a34316e92fafc17fc01f5477884f8bb2917d8
Handika Harianto Ew Jong <handikaharianto01@gmail.com>
2021-12-30 07:08:10 +0700

    Add Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from './components/home/Hero'
 import Navbar from './components/shared/Navbar'
 
 function App() {
   return (
     <>
-      <Navbar dark={true} />
+      <Navbar />
+      <Hero />
     </>
   )
 }
---
+++ src/components/home/Hero.jsx
@@ -0,0 +1,15 @@
+function Hero() {
+  return (
+    <section className='hero'>
+      <div className='container hero__container'>
+        <h1 className='hero__title'>Rest, Relax, Unwind</h1>
+        <p className='hero__desc'>Embrace your choices - we do</p>
+        <a className='hero__shop-btn' href='/products'>
+          SHOW NOW
+        </a>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
