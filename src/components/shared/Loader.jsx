1ffc908731672bcfd2e3e77db52e9706a8502dd4
Handika Harianto Ew Jong <handikaharianto01@gmail.com>
2021-12-30 12:51:19 +0700

    Add Loader component

---
+++ src/components/shared/Loader.jsx
@@ -0,0 +1,11 @@
+function Loader() {
+  return (
+    <div className='loader'>
+      <div className='big-circle'>
+        <div className='small-circle'></div>
+      </div>
+    </div>
+  )
+}
+
+export default Loader
