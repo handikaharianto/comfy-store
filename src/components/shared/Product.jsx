2f6a613e959a2b40d45e16ba36690d2021ecb099
Handika Harianto Ew Jong <handikaharianto01@gmail.com>
2021-12-30 12:44:24 +0700

    Add Product component

---
+++ src/components/shared/Product.jsx
@@ -0,0 +1,31 @@
+import { FaSearch, FaShoppingCart } from 'react-icons/fa'
+
+function Product({ id, fields }) {
+  const {
+    name,
+    price,
+    image: [{ url }],
+  } = fields
+
+  return (
+    <article className='product'>
+      <div className='product__top'>
+        <img className='product__img' src={url} alt={name} />
+        <div className='product__btns'>
+          <a className='product__details-btn' href={`/${id}`}>
+            <FaSearch />
+          </a>
+          <button className='product__cart-btn' type='button'>
+            <FaShoppingCart />
+          </button>
+        </div>
+      </div>
+      <div className='product__body'>
+        <h5 className='product__name'>{name}</h5>
+        <p className='product__price'>${price / 100}</p>
+      </div>
+    </article>
+  )
+}
+
+export default Product
